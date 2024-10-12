12bb212f27ef4f7bfc0f420665c0fd58b75c3f3b
Biniza Ruiz <binizacruuiz@gmail.com>
2024-10-11 18:54:23 -0600

    Necesidades del cliente

--- main.py
+++ main.py
@@ -1,12 +1,26 @@
 from usuario import Usuario
 import pandas as pd
 from autenticacion import login
+from necesidadCliente import datos_completos, mes_nombre, getLugar, mes_nombre, estatus_retraso
 
-JFK = pd.read_csv("./baseDeDatos/JFK.csv")
-EWR = pd.read_csv("./baseDeDatos/EWR.csv")
-LGA = pd.read_csv("./baseDeDatos/LGA.csv")
-datos_completos = pd.concat([JFK, EWR, LGA], axis = 0, ignore_index = True) 
+#Necesidades del cliente
+datos_completos = datos_completos() #4
 
+datos_completos["Meses"] = datos_completos["month"].apply(mes_nombre) #5
+
+datos_completos["Distancia_Km"] = datos_completos["distance"] * 1.60934 #6
+
+datos_completos["Tiempo_Hr"] = datos_completos["air_time"]/60 #7
+
+datos_completos["Velocidad_promedio"] = (datos_completos["Distancia_Km"]/ datos_completos["Tiempo_Hr"]).round(2) #8
+
+datos_completos["Origen"] = datos_completos["origin"].apply(getLugar) #9
+
+datos_completos["Destino"] = datos_completos["dest"].apply(getLugar)
+
+datos_completos["Status_atraso"] = datos_completos["dep_delay"].apply(estatus_retraso) #10
+
+#Menu funciones
 def contar_vuelos_origen_destino():
     print("Contabilizando la cantidad de vuelos según su origen y destino...")
 
@@ -14,7 +28,7 @@ def contar_vuelos_origen_fecha():
     lugar = input("Ingresa el origen: ")
     fecha = input("Ingresa la fecha: ")
 
-    #print(datos_completos[datos[""]])
+    #print(datos_completos[datos["origin"]==])
 
 def resumen_estadistico_vuelos_origen_variable():
     print("Realizando resumen estadístico de los vuelos según el aeropuerto de origen y la variable seleccionada...")
@@ -73,43 +87,80 @@ def mostrar_menu():
             print("Opción no válida, por favor intente de nuevo.")
             opcValida = False
 
-usuario_actual = login()
+#usuario_actual = login()
 
-print(f"Usuario actual: {usuario_actual}")
+#print(f"Usuario actual: {usuario_actual}")
 
-def mes_nombre(mes):
-    if mes == "1":
-        return "Enero"
-    elif mes == "2":
-        return "Febrero"
-    elif mes == "3":
-        return "Marzo"
-    elif mes == "4":
-        return  "Abril"
-    elif mes == "5":
-        return  "Mayo"
-    elif mes == "6":
-        return "Junio"
-    elif mes == "7":
-        return "Julio"
-    elif mes == "8":
-        return "Agosto"
-    elif mes == "9":
-        return "Septiembre"
-    elif mes == "10":
-        return "Octubre"
-    elif mes == "11":
-        return "Noviembre"
-    elif mes == "12":
-        return "Diciembre"
-    else: 
-        return "Error"
+#print(datos_completos[1:3])
+
+#Funciones a,c,e,f,x
+def contar_vuelos_origen_destino(origen, destino):
+    if origen not in datos_completos["origin"].unique():
+        print(f"Error: El origen {origen} no es válido.")
+        return
+    if destino not in datos_completos["dest"].unique():
+        print(f"Error: El destino {destino} no es válido.")
+        return
+        
+    total_vuelos = len(datos_completos.loc[(datos_completos["origin"] == origen) & (datos_completos["dest"] == destino)])
+    print(f"Total de vuelos desde {origen} a {destino}: {total_vuelos}")
+
+def resumen_estadistico_vuelos_origen_variable(origen, variable):
+    if origen not in datos_completos["origin"].unique():
+        print(f"Error: El origen {origen} no es válido.")
+        return
+    if variable not in datos_completos.columns:
+        print(f"Error: La variable {variable} no existe en los datos.")
+
+    datos = datos_completos.loc[datos_completos["origin"] == origen, variable]
+
+    if datos.empty:
+        print(f"No se encontraron datos para el origen {origen} y la variable {variable}.")
+        return
     
-datos_completos["month"] = datos_completos["Mes"].apply(mes_nombre)
-datos_completos = datos_completos.drop(columns=["month"])
+    valor_minimo = datos.min()
+    valor_maximo = datos.max()
+    media = datos.mean()
+    desviacion_estandar = datos.std()
+    print(f"El valor mínimo registrado para la variable {variable} es de: {valor_minimo} ")
+    print(f"El valor máximo registrado para la variable {variable} es de: {valor_maximo} ")
+    print(f"El valor promedio registrado para la variable {variable} es de: {media} ")
+    print(f"El valor de la desviación estándar para la variable {variable} es de: {desviacion_estandar} ")
 
-def distance_mi_km():
-    datos_completos["Distancia_Km"] = datos_completos["distance"] * 1.60934
+#Tienes que ver si hay años bisiestos, y corregir lo de los meses
+def resumen_criterio_fecha(criterio, mes, dia):
+    dias_meses = {
+        1:31,
+        2:28,
+        3:31,
+        4:30,
+        5:31,
+        6:30,
+        7:31,
+        8:31,
+        9:30,
+        10:31,
+        11:30,
+        12:31,
+    }
 
-def airTime_min_hrs():
-    datos_completos["Tiempo_Hr"] = datos_completos["air_time"]/60
+    if mes <1 or mes >12:
+        print("El valor del mes es inválido.")
+        return
+    
+    if dia <1 or dia > dias_meses[mes]:
+        print(f"El valor del día es inválido para el mes {mes}.")
+        return
+    
+    datos = datos_completos[(datos_completos["month"] == mes) & (datos_completos["day"] == dia)]
+
+    if criterio == "Tiempo_Hr":
+        tiempo = datos["Tiempo_Hr"].sum()
+        print(f"El total de horas de todos los vuelos realizados el {dia} de {mes} es de: {tiempo}.")
+    
+    elif criterio == "Distancia_Km":
+        distancia = datos["Distancia_Km"].sum()
+        print(f"El total de kilómetros de todos los vuelos realizados el {dia} de {mes} es de {distancia}.")
+    else:
+       print(f"El criterio debe ser 'Distancia_Km' o 'Tiempo_Hr'.")
+       return 
---
+++ necesidadCliente.py
@@ -0,0 +1,150 @@
+import pandas as pd
+
+lugarMap = {
+    "JFK": "Jhon F.Kennedy",
+    "EWR": "Newark",
+    "LGA": "LaGuardia",
+    "ABQ": "Albuquerque",
+    "ACK": "Nantucket",
+    "ALB": "Albany",
+    "ANC": "Anchorage",
+    "ATL": "Atlanta", 
+    "AUS": "Austin",
+    "AVL": "Asheville",
+    "BDL": "Bradley",
+    "BGR": "Bangor",
+    "BHM": "Birmingham",
+    "BNA": "Nashville",
+    "BOS": "Boston", 
+    "BTV": "Burlington",
+    "BUF": "Buffalo",
+    "BUR": "Burbank",
+    "BWI": "Baltimore",
+    "BZN": "Bozeman",
+    "CAK": "Akron-Canton",
+    "CAE": "Columbia",
+    "CHAR": "Charlottesville",
+    "CHO": "Charlottesville",
+    "CHS": "Charleston",
+    "CLE": "Cleveland",
+    "CLT": "Charlotte",
+    "CMH": "Columbus",
+    "CRW": "Charleston (West Virginia)",
+    "CVG": "Cincinnati",
+    "DAY": "Dayton",
+    "DCA": "Washington D.C.",
+    "DEN": "Denver",
+    "DFW": "Dallas/Fort Worth",
+    "DSM": "Des Moines",
+    "DTW": "Detroit",
+    "EGE": "Eagle",
+    "EYW": "Key West",
+    "FLL": "Fort Lauderdale",
+    "GRR": "Grand Rapids",
+    "GSO": "Greensboro",
+    "GSP": "Greenville",
+    "HDN": "Hayden",
+    "HNL": "Honolulu",
+    "HOU": "Houston",
+    "IAD": "Washington Dulles",
+    "IAH": "Houston Intercontinental",
+    "ILM": "Wilmington",
+    "IND": "Indianapolis",
+    "JAC": "Jackson Hole",
+    "JAX": "Jacksonville",
+    "LAX": "Los Ángeles",
+    "LEX": "Lexington",
+    "LGA": "LaGuardia",
+    "LGB": "Long Beach",
+    "LAS": "Las Vegas",
+    "MCI": "Kansas City",
+    "MCO": "Orlando",
+    "MDW": "Chicago Midway",
+    "MEM": "Memphis",
+    "MHT": "Manchester",
+    "MIA": "Miami",
+    "MKE": "Milwaukee",
+    "MSN": "Madison",
+    "MSP": "Minneapolis",
+    "MSY": "Nueva Orleans",
+    "MTJ": "Montrose",
+    "MVY": "Martha's Vineyard",
+    "MYR": "Myrtle Beach",
+    "NASH": "Nashville",
+    "OAK": "Oakland",
+    "OKC": "Oklahoma City",
+    "OMA": "Omaha",
+    "ORD": "Chicago O'Hare",
+    "ORF": "Norfolk",
+    "PBI": "Palm Beach",
+    "PDX": "Portland",
+    "PHL": "Philadelphia",
+    "PHX": "Phoenix",
+    "PIT": "Pittsburgh",
+    "PSE": "Ponce",
+    "PSP": "Palm Springs",
+    "PVD": "Providence",
+    "PWM": "Portland (Maine)",
+    "RDU": "Raleigh-Durham",
+    "RIC": "Richmond",
+    "ROC": "Rochester",
+    "RSW": "Fort Myers",
+    "SAN": "San Diego",
+    "SAT": "San Antonio",
+    "SAV": "Savannah",
+    "SBN": "South Bend",
+    "SDF": "Louisville",
+    "SEA": "Seattle",
+    "SFO": "San Francisco",
+    "SJC": "San José (California)",
+    "SJU": "San Juan",
+    "SLC": "Salt Lake City",
+    "SMF": "Sacramento",
+    "SNA": "Santa Ana",
+    "SRQ": "Sarasota",
+    "STL": "St. Louis",
+    "STT": "St. Thomas",
+    "SYR": "Syracuse",
+    "TUL": "Tulsa",
+    "TPA": "Tampa",
+    "TVC": "Traverse City",
+    "TYS": "Knoxville",
+    "XNA": "Northwest Arkansas",
+    "BQN": "Aguadilla"
+}
+
+meses = {
+    1: "Enero",
+    2: "Febrero",
+    3: "Marzo",
+    4: "Abril",
+    5: "Mayo",
+    6: "Junio",
+    7: "Julio",
+    8: "Agosto",
+    9: "Septiembre",
+    10: "Octubre",
+    11: "Noviembre",
+    12: "Diciembre"
+}
+
+def datos_completos():
+    JFK = pd.read_csv("./baseDeDatos/JFK.csv")
+    EWR = pd.read_csv("./baseDeDatos/EWR.csv")
+    LGA = pd.read_csv("./baseDeDatos/LGA.csv")
+    
+    return pd.concat([JFK, EWR, LGA], axis = 0, ignore_index = True) 
+
+def getLugar(lugar):
+    return lugarMap[lugar]
+
+def mes_nombre(mes):
+    return meses[mes]
+
+def estatus_retraso(estatus):
+    if estatus < 0:
+        return "Vuelo anticipado"
+    elif estatus > 0:
+        return "Vuelo anticipado"
+    else:
+        return "Vuelo en tiempo"
